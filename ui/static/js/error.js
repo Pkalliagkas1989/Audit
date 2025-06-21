ba7521f756e38068525a0d2462d733e30c19220e
mkouvara <marinoskouvaras@gmail.com>
2025-06-22 00:52:12 +0300

    error captured

--- ui/static/js/error.js
+++ ui/static/js/error.js
@@ -1,17 +1,18 @@
+// /static/js/error.js
+
 window.addEventListener('DOMContentLoaded', () => {
   const params = new URLSearchParams(window.location.search);
   const msg = params.get('msg');
+  const code = params.get('code');
 
   const errorMessage = document.getElementById('error-message');
+  const errorCode = document.getElementById('error-code');
 
-  if (msg && errorMessage) {
-    errorMessage.textContent = decodeURIComponent(msg);
-  } else if (errorMessage) {
-    errorMessage.textContent = "An unexpected error occurred.";
+  if (errorCode) {
+    errorCode.textContent = code ? `Error ${code}` : 'Error';
   }
 
-  document.getElementById("back-button")?.addEventListener("click", () => {
-    window.history.back();
-  });
+  if (errorMessage) {
+    errorMessage.textContent = msg ? decodeURIComponent(msg) : "An unexpected error occurred.";
+  }
 });
-// 
